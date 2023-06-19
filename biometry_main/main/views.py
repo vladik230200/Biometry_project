38b539fbdc59808a7c570b20276a02303fdb8918
vladik230200 <karelov12345678@mail.ru>
2023-06-19 21:31:34 +0300

    Test

--- biometry_main/main/views.py
+++ biometry_main/main/views.py
@@ -6,11 +6,7 @@ from django.http import JsonResponse
 from main.models import users, samples
 import hashlib
 import string
-import io
-import wave
-import librosa
-import scipy.io.wavfile as wavfile
-import numpy as np
+from django.core.files.storage import default_storage
 
 def critical(request):
     render(request, "main/critical_404.html")
@@ -54,46 +50,6 @@ def verification(request):
         for i in range(len(audio)):
             sample = audio["audio" + str(i + 1)]
             sample_massiv.append(sample)
-        for i, sample_blob in enumerate(sample_massiv):
-            # Прочитать данные из объекта InMemoryUploadedFile
-            data = sample_blob.read()
-            # Уменьшить размер данных до ближайшего кратного размеру элемента
-            data_size = len(data)
-            element_size = np.dtype(np.int16).itemsize
-            new_data_size = data_size - (data_size % element_size)
-            data = data[:new_data_size]
-            # Преобразовать данные в формат wav и сохранить их в файл
-            sr = 16000
-            wav_data = np.frombuffer(data, dtype=np.int16)
-            wavfile.write("wave" + str(i) + ".wav", sr, wav_data)
-
-        """
-        for i in range(len(audio)):
-            audio_data = audio["audio" + str(i+1)]
-            audio_segment = AudioSegment.from_file(io.BytesIO(audio_data.read()))
-            audio_segment.export('audio.wav', format='wav')
-        """
-
-        """
-        for i in range(len(audio)):
-            audio_data = audio["audio" + str(i + 1)].read()  # читаем данные из InMemoryUploadedFile
-            sample = memoryview(audio_data).cast('B')  # преобразуем в байты
-            print(sample)
-            with wave.open('audio' + str(i + 1) + '.wav', 'wb') as wav_file:
-                # Устанавливаем параметры записи: монофонический звук, частота дискретизации 44100 Гц, 16 бит на сэмпл
-                wav_file.setnchannels(1)
-                wav_file.setframerate(44100)
-                wav_file.setsampwidth(2)
-                # Запись данных в файл
-                wav_file.writeframes(audio_data)
-                # Запись заголовка WAV-файла
-                wav_file.writeframesraw(b'')
-                # Закрытие файла
-                wav_file.close()
-        """
-
-
-
         login_user_check = users.objects.filter(username=login_user)
         if len(login_user) <= 0 or len(password_user) <= 0 or len(sample_massiv) < 3:
             data = {"redirect_url" : "registration/verification/critical"}
